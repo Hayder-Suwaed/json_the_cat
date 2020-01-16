ccf4a11db73c8cbedf07c10e2d0d09188b197e85
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-16 22:01:34 +0000

    modify breedFectch file, add index and add test function files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
-const request = require("request");
-const input = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${input}`;
-request(url, (error, resp, body) => {
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Name is not Exist");
-    process.exit();
-  } else {
-    console.log(data[0][`description`]);
-  }
-});
+const request = require('request');
+const url = `https://api.thecatapi.com/v1/breeds/search?q=`;
+const fetchBreedDescription = function(breedName, callback) {
+  request(url + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data['status'] === 404) {
+      callback(error);
+      process.exit();
+    } if (data[0] === undefined) {
+      callback('No!!! such a Breed' , null );
+    } else {
+      callback(null , data[0].description);
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
