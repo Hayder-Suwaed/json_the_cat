c4f4f97162a6dd0d8cae556f6643907beee6a842
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-16 23:13:15 +0000

    json_the_cat

--- iss-spotter/README.md
+++
@@ -1,3 +0,0 @@
-## ISS_Spotter assignment from W2D4 at Ligththouse Labs Bootcamp
-
-
--- iss-spotter/index2.js
+++
@@ -1,7 +0,0 @@
-const { nextISSTimesForMyLocation } = require('./iss_promised');
-nextISSTimesForMyLocation()
-    .then((passTimes) => {
-        for (let timeof ),
-      console.log(PassTimes(passTimes));
-    })nextISSTimesForMyLocationc
-  
